90366f01fc6e45624ddb3740a258bb0d41df7d63
BarzometrK <72938018+BarzometrK@users.noreply.github.com>
2020-12-16 18:22:53 +0300

    Выбор нескольких/всех комментариев

---
+++ USER.BOT-main/USER.BOT/USER.BOT/AnswerForm.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace USER.BOT
+{
+    partial class AnswerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.ID1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Comments = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Text1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.buttonInput = new System.Windows.Forms.Button();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.ID2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.parentID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.FirstName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.LastName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Text2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelOutput = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.SystemColors.Control;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ID1,
+            this.Comments,
+            this.Text1});
+            this.listView1.FullRowSelect = true;
+            this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listView1.HideSelection = false;
+            this.listView1.LargeImageList = this.imageList1;
+            this.listView1.Location = new System.Drawing.Point(12, 32);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(620, 150);
+            this.listView1.SmallImageList = this.imageList1;
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            this.listView1.Enter += new System.EventHandler(this.listView1_Enter);
+            // 
+            // ID1
+            // 
+            this.ID1.Text = "ID";
+            this.ID1.Width = 52;
+            // 
+            // Comments
+            // 
+            this.Comments.Text = "Комментарии";
+            this.Comments.Width = 85;
+            // 
+            // Text1
+            // 
+            this.Text1.Text = "Текст";
+            this.Text1.Width = 380;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth16Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.BackColor = System.Drawing.SystemColors.Control;
+            this.textBoxInput.Enabled = false;
+            this.textBoxInput.Location = new System.Drawing.Point(404, 383);
+            this.textBoxInput.Multiline = true;
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(260, 27);
+            this.textBoxInput.TabIndex = 2;
+            this.textBoxInput.TextChanged += new System.EventHandler(this.textBoxInput_TextChanged);
+            // 
+            // buttonInput
+            // 
+            this.buttonInput.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.buttonInput.ForeColor = System.Drawing.SystemColors.Control;
+            this.buttonInput.Location = new System.Drawing.Point(404, 445);
+            this.buttonInput.Name = "buttonInput";
+            this.buttonInput.Size = new System.Drawing.Size(185, 23);
+            this.buttonInput.TabIndex = 3;
+            this.buttonInput.Text = "Поиск";
+            this.buttonInput.UseVisualStyleBackColor = false;
+            this.buttonInput.Click += new System.EventHandler(this.buttonInput_Click);
+            // 
+            // listView2
+            // 
+            this.listView2.BackColor = System.Drawing.SystemColors.Control;
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ID2,
+            this.parentID,
+            this.FirstName,
+            this.LastName,
+            this.Text2});
+            this.listView2.FullRowSelect = true;
+            this.listView2.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listView2.HideSelection = false;
+            this.listView2.LargeImageList = this.imageList1;
+            this.listView2.Location = new System.Drawing.Point(12, 208);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(776, 169);
+            this.listView2.SmallImageList = this.imageList1;
+            this.listView2.TabIndex = 6;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            this.listView2.SelectedIndexChanged += new System.EventHandler(this.listView2_SelectedIndexChanged);
+            this.listView2.Enter += new System.EventHandler(this.listView2_Enter);
+            // 
+            // ID2
+            // 
+            this.ID2.Text = "ID";
+            this.ID2.Width = 46;
+            // 
+            // parentID
+            // 
+            this.parentID.Text = "ID ответ";
+            this.parentID.Width = 61;
+            // 
+            // FirstName
+            // 
+            this.FirstName.Text = "Имя";
+            this.FirstName.Width = 91;
+            // 
+            // LastName
+            // 
+            this.LastName.Text = "Фамилия";
+            this.LastName.Width = 167;
+            // 
+            // Text2
+            // 
+            this.Text2.Text = "Текст";
+            this.Text2.Width = 332;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.SystemColors.Control;
+            this.progressBar1.Location = new System.Drawing.Point(404, 416);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(384, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 7;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(638, 32);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // labelOutput
+            // 
+            this.labelOutput.AutoSize = true;
+            this.labelOutput.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.labelOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelOutput.ForeColor = System.Drawing.SystemColors.Control;
+            this.labelOutput.Location = new System.Drawing.Point(12, 380);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(379, 15);
+            this.labelOutput.TabIndex = 9;
+            this.labelOutput.Text = "Нажмите на кнопку для вывода всех записей с вашей страницы";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(12, 185);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Комментарии:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 20);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Посты:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(634, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 20);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Фото:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Control;
+            this.label4.Location = new System.Drawing.Point(670, 385);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 20);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Осталось:";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.Enabled = false;
+            this.button1.ForeColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(604, 445);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Выбрать все комментарии";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // AnswerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.ClientSize = new System.Drawing.Size(800, 476);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelOutput);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.buttonInput);
+            this.Controls.Add(this.textBoxInput);
+            this.MaximumSize = new System.Drawing.Size(816, 515);
+            this.MinimumSize = new System.Drawing.Size(816, 515);
+            this.Name = "AnswerForm";
+            this.Text = "Ответ на комментарии";
+            this.Load += new System.EventHandler(this.AnswerForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Button buttonInput;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ColumnHeader ID2;
+        private System.Windows.Forms.ColumnHeader parentID;
+        private System.Windows.Forms.ColumnHeader FirstName;
+        private System.Windows.Forms.ColumnHeader LastName;
+        private System.Windows.Forms.ColumnHeader Text2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labelOutput;
+        private System.Windows.Forms.ColumnHeader ID1;
+        private System.Windows.Forms.ColumnHeader Comments;
+        private System.Windows.Forms.ColumnHeader Text1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ USER.BOT-main/USER.BOT/USER.BOT/AnswerForm.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using Newtonsoft.Json;
+using System.Threading;
+
+namespace USER.BOT
+{
+    public partial class AnswerForm : Form
+    {
+        public string access_token;
+        public string user_id;
+        public string Request;
+        public WebClient cl;
+        public string Answer;
+        public WallGet wg;
+        public string postID;
+        public string commentID;
+        public int target = 0;
+        public int postCount;
+        public int progress;
+        public int stage;
+
+        public AnswerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AnswerForm_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private string GetAnswer(string Request, string AccessToken)
+        {
+            string Req = Request + AccessToken + "&v=5.124";
+            cl = new WebClient();
+            Answer = Encoding.UTF8.GetString(cl.DownloadData(Req));
+            return Answer;
+        }
+
+        private void buttonInput_Click(object sender, EventArgs e)
+        {
+            if (stage == 0)
+            {
+                OutputPosts();
+            }
+
+            if (stage == 2)
+            {
+                WorkWithComments();
+            }
+        }
+
+        private void OutputPosts()
+        {
+            stage = 1;
+            Request = "https://api.vk.com/method/wall.get?";
+            Answer = GetAnswer(Request, access_token);
+            wg = JsonConvert.DeserializeObject<WallGet>(Answer);
+
+            labelOutput.Text = "Пожалуйста подождите...";
+            buttonInput.Enabled = false;
+            listView1.Enabled = false;
+            listView2.Enabled = false;
+            Select();
+
+            foreach (WallGet.Item itemWG in wg.response.items)
+            {
+                if (itemWG.comments.count > 0)
+                {
+                    progress++;
+                }
+            }
+
+            progressBar1.Maximum = progress;
+            progressBar1.Value = progressBar1.Maximum;
+            label4.Text = "Осталось: " + progressBar1.Value.ToString();
+
+            foreach (WallGet.Item itemWG in wg.response.items)
+            {
+                if (itemWG.comments.count > 0)
+                {
+                    string[] LVitem = new string[4];
+                    LVitem[0] = itemWG.id.ToString();
+                    LVitem[1] = itemWG.comments.count.ToString();
+                    LVitem[2] = itemWG.text;
+                    if (itemWG.attachments != null)
+                    {
+                        foreach (WallGet.Attachment attachmentsWG in itemWG.attachments)
+                        {
+                            if (attachmentsWG.type == "photo")
+                            {
+                                LVitem[3] = itemWG.attachments[0].photo.sizes[0].url;
+                            }
+                        }
+                    }
+
+                    ListViewItem lvi = new ListViewItem(LVitem);
+                    listView1.Items.Add(lvi);
+                    progressBar1.Value--;
+                    label4.Text = "Осталось: " + progressBar1.Value.ToString();
+                }
+                Application.DoEvents();
+                Thread.Sleep(500);
+            }
+
+            labelOutput.Text = "Найдено "+ progress + " записи с комментариями" + "\r\n" +
+                "Выберите запись, комментарии к которой хотите увидеть, " + "\r\n" +
+                "нажав на соответствующее ID, и нажмите на кнопку" + "\r\n" + "для вывода";
+            buttonInput.Text = "Вывод";
+            listView1.Enabled = true;
+            listView2.Enabled = true;
+        }
+
+        private void WorkWithComments()
+        {
+            if (target == 1)
+            {
+                OutputComments();
+            }
+            else
+            {
+                AnswerToComment();
+            }
+        }
+
+        private void OutputComments()
+        {
+            progressBar1.Maximum = progress;
+            progressBar1.Value = progressBar1.Maximum;
+            label4.Text = "Осталось: " + progressBar1.Value.ToString();
+
+            if (labelOutput.Text != "")
+            {
+                WallGet.Item itemWG = new WallGet.Item();
+                itemWG.id = Convert.ToInt16(postID);
+                Request = "https://api.vk.com/method/wall.getComments?owner_id=" + user_id + "&post_id=" + itemWG.id + "&";
+                Answer = GetAnswer(Request, access_token);
+                CommentsGet csg = JsonConvert.DeserializeObject<CommentsGet>(Answer);
+                listView2.Items.Clear();
+
+                labelOutput.Text = "Пожалуйста подождите...";
+                buttonInput.Enabled = false;
+                listView1.Enabled = false;
+                listView2.Enabled = false;
+                Select();
+
+                foreach (CommentsGet.Item itemCG in csg.response.items)
+                {
+                    string[] LVitem2 = new string[5];
+
+                    Request = "https://api.vk.com/method/users.get?user_ids=" + itemCG.from_id + "&";
+                    Answer = GetAnswer(Request, access_token);
+                    UsersGet ug = JsonConvert.DeserializeObject<UsersGet>(Answer);
+
+                    Application.DoEvents();
+
+                    LVitem2[0] = itemCG.id.ToString();
+                    LVitem2[2] = ug.response[0].first_name;
+                    LVitem2[3] = ug.response[0].last_name;
+                    LVitem2[4] = itemCG.text;
+
+                    ListViewItem lvi2 = new ListViewItem(LVitem2);
+                    listView2.Items.Add(lvi2);
+
+                    string id = itemCG.id.ToString();
+                    Request = "https://api.vk.com/method/wall.getComments?owner_id=" + user_id + "&post_id=" + itemWG.id + 
+                        "&comment_id=" + id + "&";
+                    Answer = GetAnswer(Request, access_token);
+                    CommentsGet2 csg2 = JsonConvert.DeserializeObject<CommentsGet2>(Answer);
+
+                    Thread.Sleep(600);
+
+                    progressBar1.Value--;
+                    label4.Text = "Осталось: " + progressBar1.Value.ToString();
+
+                    foreach (CommentsGet2.Item itemCsG2 in csg2.response.items)
+                    {
+                        Request = "https://api.vk.com/method/users.get?user_ids=" + itemCsG2.from_id + "&";
+                        Answer = GetAnswer(Request, access_token);
+                        ug = JsonConvert.DeserializeObject<UsersGet>(Answer);
+
+                        Application.DoEvents();
+
+                        LVitem2[0] = itemCsG2.id.ToString();
+                        LVitem2[1] = id.ToString();
+                        LVitem2[2] = ug.response[0].first_name;
+                        LVitem2[3] = ug.response[0].last_name;
+                        LVitem2[4] = itemCsG2.text;
+
+                        lvi2 = new ListViewItem(LVitem2);
+                        listView2.Items.Add(lvi2);
+
+                        Thread.Sleep(600);
+
+                        progressBar1.Value--;
+                        label4.Text = "Осталось: " + progressBar1.Value.ToString();
+                    }
+                }
+            }
+            labelOutput.Text = "Выберите комментарий, ответ на который хотите отправить" + "\r\n" +
+                "Для этого нажмите на ID соответствующего комментария" + "\r\n" +
+                "Вы можете выбрать несколько, удерживая Ctrl или Shift, или" + "\r\n" + 
+                "выбрать все с помощью кнопки" + "\r\n" +
+                "Либо выберите другую запись";
+            buttonInput.Enabled = true;
+            button1.Enabled = true;
+            listView1.Enabled  = true;
+            listView2.Enabled = true;
+        }
+
+        private void AnswerToComment()
+        {
+            if (textBoxInput.Text != "")
+            {
+                labelOutput.Text = "Пожалуйста подождите...";
+                foreach (ListViewItem lwi in listView2.SelectedItems)
+                {
+                    commentID = lwi.Text;
+                    Request = "https://api.vk.com/method/wall.createComment?owner_id=" + user_id + "&post_id=" + postID +
+                        "&message=" + textBoxInput.Text + "&reply_to_comment=" + commentID + "&";
+                    Answer = GetAnswer(Request, access_token);
+
+                    foreach (ListViewItem lvi in listView1.Items)
+                    {
+                        if (lvi.SubItems[0].Text == postID)
+                        {
+                            lvi.SubItems[1].Text = Convert.ToString(Convert.ToInt32(lvi.SubItems[1].Text) + 1);
+                        }
+                    }
+                    Thread.Sleep(600);
+                }
+
+                labelOutput.Text = "Комментарий(ии) отправлен(ы)";
+                textBoxInput.Text = "";
+                progress = 0;
+            }
+            else
+            {
+                labelOutput.Text = "Введите ответ на комментарий(ии)";
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                labelOutput.Text = "Выбрана запись с ID: " + listView1.SelectedItems[0].Text + "\r\n" +
+                    "Нажмите кнопку для вывода комментариев";
+                postID = listView1.SelectedItems[0].Text;
+
+                progress = Convert.ToInt32(listView1.SelectedItems[0].SubItems[1].Text);
+                
+                target = 1;
+
+                if (listView1.SelectedItems[0].SubItems[3].Text != "")
+                {
+                    pictureBox1.ImageLocation = listView1.SelectedItems[0].SubItems[3].Text;
+                }
+                else
+                {
+                    pictureBox1.Image = null;
+                }
+
+                buttonInput.Text = "Вывод";
+                textBoxInput.Enabled = false;
+                stage = 2;
+                buttonInput.Enabled = true;
+                button1.Enabled = false;
+            }
+        }
+
+        private void listView2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView2.SelectedItems.Count > 0)
+            {
+                labelOutput.Text = "Выбран(ы) комментарий(ии) с ID: ";
+                foreach (ListViewItem lvi in listView2.SelectedItems)
+                {
+                    labelOutput.Text = labelOutput.Text + lvi.Text + " ";
+                }
+                labelOutput.Text = labelOutput.Text + "\r\n" +
+                    "Введите текст ответа и нажмите кнопку для отправки";
+
+                target = 2;
+                textBoxInput.Enabled = true;
+
+                button1.Text = "Убрать выделение";
+            }
+            else
+            {
+                button1.Text = "Выбрать все комментарии";
+            }
+        }
+
+        private void textBoxInput_TextChanged(object sender, EventArgs e)
+        {
+            buttonInput.Text = "Отправить";
+        }
+
+        private void listView1_Enter(object sender, EventArgs e)
+        {
+            listView1_SelectedIndexChanged(sender, e);
+        }
+
+        private void listView2_Enter(object sender, EventArgs e)
+        {
+            listView2_SelectedIndexChanged(sender, e);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "Выбрать все комментарии")
+            {
+                foreach (ListViewItem lvi in listView2.Items)
+                {
+                    lvi.Selected = true;
+                    button1.Text = "Убрать выделение";
+                }
+            }
+            else if (button1.Text == "Убрать выделение")
+            {
+                foreach (ListViewItem lvi in listView2.Items)
+                {
+                    lvi.Selected = false;
+                    button1.Text = "Выбрать все комментарии";
+                }
+            }
+        }
+    }
+}
